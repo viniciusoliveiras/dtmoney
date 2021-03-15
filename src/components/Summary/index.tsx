7988c4d5a77ea5d58e62c4cb4971e7cd66f8131f
viniciusoliveiras <vinitag190@gmail.com>
2021-03-15 14:08:56 -0300

    Componentizacao | Componente: Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Income</p>
+          <img src={incomeImg} alt='Income' />
+        </header>
+        <strong>$1000.00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Outcome</p>
+          <img src={outcomeImg} alt='Outcome' />
+        </header>
+        <strong>-$200.00</strong>
+      </div>
+
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={total} alt='Total' />
+        </header>
+        <strong>$800.00</strong>
+      </div>
+    </Container>
+  );
+}
