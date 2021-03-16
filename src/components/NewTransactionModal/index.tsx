dc96bdd626e0b70f1d67bf6c68b2233e36931a45
viniciusoliveiras <vinitag190@gmail.com>
2021-03-15 22:08:54 -0300

    Modal & Forms | Estilizando modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-// import closeImg from '../../assets/close.svg';
+import closeImg from '../../assets/close.svg';
 import { Container } from './styles';
 
 interface NewTransactionModalProps {
@@ -18,12 +18,23 @@ export function NewTransactionModal({
       overlayClassName='react-modal-overlay'
       className='react-modal-content'
     >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <img src={closeImg} alt='Close' />
+      </button>
+
       <Container>
         <h2>Register transaction</h2>
 
         <input placeholder='Title' />
+
         <input placeholder='Price' type='number' />
+
         <input placeholder='Category' />
+
         <button type='submit'>Register</button>
       </Container>
     </Modal>
