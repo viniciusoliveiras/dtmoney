8afddab280811d7f1ec54c431b5e34ba56346508
viniciusoliveiras <vinitag190@gmail.com>
2021-03-19 11:07:02 -0300

    Modal & Forms | Inserindo transactions na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -22,12 +23,14 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       type,
       category,
-    });
+    };
+
+    api.post('/transaction', data)
   }
 
   return (
